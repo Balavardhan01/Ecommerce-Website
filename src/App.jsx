10eb08e37082ec9ec23888654900a4f7e6018548
Balavardhan Katta <balavardhankatta01@gmail.com>
2025-12-31 21:56:35 +0530

    adding src files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl bg-cyan-400 text-white px-4 py-3 flex content-center justify-center mt-30'>Ecommerce</h1>
+    </div>
+  )
+}
+
+export default App
