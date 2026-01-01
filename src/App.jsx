b329661f284e28cb39a80e05b59ac9a31522f290
Balavardhan Katta <balavardhankatta01@gmail.com>
2026-01-01 18:48:38 +0530

    updated the UI and Navbar in HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import LandingPage from './stores/pages/LandingPage'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
   return (
   <>
-    <LandingPage/>
+    <Routes>
+      <Route path='/' element={<LandingPage/>}/>
+      
+    </Routes>
   </>
   )
 }
--- src/stores/components/Navbar.jsx
+++ src/stores/components/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Search } from 'lucide-react';
 
 const Navbar = () => {
   return (
     <>
-    <div className='flex justify-around mb-1  p-3 border-gray-800 items-center bg-cyan-600 border '>
+    <div className='flex justify-around   p-3 border-gray-800 items-center bg-cyan-700 border '>
        <div>
          <h2 className='text-3xl text-white'>E-Grab</h2>
        </div>
 
-       <div>
-        <input className='bg-sky-100 rounded-xl outline-0 px-3 py-2' type="text"  placeholder='  Search..' />
-       </div>
+      <div className="relative w-full max-w-sm">
+         <input
+          type="text"
+          placeholder="Search..."
+          className="w-full bg-sky-100 rounded-xl outline-none pl-10 pr-3 py-2"
+           />
+          <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500" />
+      </div>
+
 
        <div>
         <div>SignIn/SignUp</div>
@@ -19,19 +26,19 @@ const Navbar = () => {
 
       </div>
 
-      <div>
-          <ul>
-            <li>Mobiles</li>
-            <li>Laptops</li>
-            <li>Mens Fashion</li>
-            <li>Watches</li>
-            <li>Womans Fashion</li>
-            <li>Tv's</li>
-            <li>Kitchen</li>
-            <li>Refrigerators</li>
-            <li>Furniture</li>
-            <li>AC</li>
-            <li>Books</li>
+      <div className=' bg-gray-700 text-white '>
+          <ul className='flex h-15 items-center'>
+            <li className='m-5'>Mobiles</li>
+            <li className='m-5'>Laptops</li>
+            <li className='m-5'>Mens Fashion</li>
+            <li className='m-5'>Watches</li>
+            <li className='m-5'>Womans Fashion</li>
+            <li className='m-5'>Tv's</li>
+            <li className='m-5'>Kitchen</li>
+            <li className='m-5'>Refrigerators</li>
+            <li className='m-5'>Furniture</li>
+            <li className='m-5'>AC</li>
+            <li className='m-5'>Books</li>
           </ul>
         </div>
         </>
